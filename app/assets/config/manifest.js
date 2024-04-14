3f97a08c223d6566200484c64eb828b543caf5cd
dubians <bamboojx@163.com>
2024-04-14 21:03:12 +0800

    update manifest.js for add dirs

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,5 +2,5 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 
-//= link bootstrap-5.3.3/package/dist/js/bootstrap.esm.js
-//= link core-2.11.8/package/dist/esm/index.js
+//= link ../../../vendor/javascript/bootstrap-5.3.3/package/dist/js/bootstrap.esm.js
+//= link ../../../vendor/javascript/core-2.11.8/package/dist/esm/index.js
