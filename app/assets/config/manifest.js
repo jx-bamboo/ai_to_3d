d47d7e40dfea580837bbdd5034fd9c6c4d1208eb
dubians <bamboojx@163.com>
2024-04-14 17:22:46 +0800

    Fix vendor assets links for deployment

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,5 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
-//= link_tree ../../../vendor/javascript
+//= link_tree ../../../vendor/javascript/bootstrap-5.3.3
+//= link_tree ../../../vendor/javascript/core-2.11.8
