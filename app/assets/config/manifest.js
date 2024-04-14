ed51c815bae534e1d9ce1f26324805663ed0790e
dubians <bamboojx@163.com>
2024-04-14 16:23:25 +0800

    change manifest.js

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,7 +1,6 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
-/* ////= link_tree ../../../vendor/javascript .js */
 
 //= link_tree ../../../vendor/javascript/bootstrap-5.3.3 .js
 //= link_tree ../../../vendor/javascript/core-2.11.8 .js
