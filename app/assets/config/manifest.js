b905402ba2f22c96a3c6894fe12f2d25debadfe8
dubians <bamboojx@163.com>
2024-04-14 21:00:54 +0800

    update manifest.js for link

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,6 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
-//= link_tree ../../../vendor/javascript/bootstrap-5.3.3/package
-//= link_tree ../../../vendor/javascript/core-2.11.8/package
+
+//= link bootstrap-5.3.3/package/dist/js/bootstrap.esm.js
+//= link core-2.11.8/package/dist/esm/index.js
