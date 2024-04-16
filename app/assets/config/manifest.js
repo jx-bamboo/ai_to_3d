99fe56368f370809132c617cdb9f41138dbe9c57
dubians <bamboojx@163.com>
2024-04-16 10:58:18 +0800

    update manifest.js add link_tree

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,5 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+//= link_tree ../../../vendor/javascript/bootstrap-5.3.3/package/dist/js .js
+//= link_tree ../../../vendor/javascript/core-2.11.8/package/dist/esm .js
