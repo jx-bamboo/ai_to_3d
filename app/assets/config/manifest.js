c321900da9f4e970abcc8f5ec697fa2121cc3f38
dubians <bamboojx@163.com>
2024-04-16 12:01:36 +0800

    delete application.js core

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,5 +2,3 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
-//= link_tree ../../../vendor/javascript/bootstrap-5.3.3/package/dist/js .js
-//= link_tree ../../../vendor/javascript/core-2.11.8/package/dist/esm .js
