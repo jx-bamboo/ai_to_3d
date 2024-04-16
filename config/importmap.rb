2874ab80e7fd854069551c7ed82163b822f10d64
dubians <bamboojx@163.com>
2024-04-16 12:10:52 +0800

    update manifest.js delete vendor&update importmap bootstrap to

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,5 +7,8 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 
 # customer
-pin "bootstrap", to: "bootstrap-5.3.3/package/dist/js/bootstrap.esm.js"
-pin "@popperjs/core", to: "core-2.11.8/package/dist/esm/index.js"
+# pin "bootstrap", to: "bootstrap-5.3.3/package/dist/js/bootstrap.esm.js"
+# pin "@popperjs/core", to: "core-2.11.8/package/dist/esm/index.js"
+#
+pin "bootstrap", to: "https://unpkg.com/bootstrap@5.3.2/dist/js/bootstrap.esm.js"
+pin "@popperjs/core", to: "https://unpkg.com/@popperjs/core@2.11.8/dist/esm/index.js"
